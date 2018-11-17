c7d32b0322134c6604b7b14cfaecc3b7c5c37f33
12Cy <christian.sandkaemper@gmail.com>
2018-11-17 21:52:14 +0100

    added startscene to project

---
+++ Assets/Scripts/BackgroundAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BackgroundAnimation : MonoBehaviour {
+
+    [SerializeField]
+    private AnimationCurve _alphaCurve;
+    [SerializeField]
+    private float _circleTime;
+    [SerializeField]
+    private float _minAlpha = 0.0f;
+    [SerializeField]
+    private float _maxAlpha = 1.0f;
+    private float _curTime;
+
+
+    [SerializeField]
+    private TextMeshProUGUI[] _text;
+
+    private void Start()
+    {
+        _curTime = 0;
+    }
+
+    private void Update()
+    {
+        _curTime += Time.deltaTime;
+
+        float t = _curTime / _circleTime;
+
+
+        for (int i = 0; i < _text.Length; i++)
+        {
+            Color c = _text[i].color;
+            c.a = _alphaCurve.Evaluate(t) * (_maxAlpha - _minAlpha) + _minAlpha;
+
+            _text[i].color = c;
+
+        }
+    }
+}
