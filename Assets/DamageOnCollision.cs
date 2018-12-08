6a6fcbb26bacce10bf48e36f90c4202aa16fc807
HanHan <hauffej@yahoo.de>
2018-12-08 20:58:58 +0100

    Level 01 Assets

---
+++ Assets/DamageOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnCollision : MonoBehaviour {
+
+    [SerializeField]
+    private int _damage;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Character chr = collision.gameObject.GetComponent<Character>();
+        if(chr)
+        {
+            chr.ApplyDamage(_damage);
+            Destroy(gameObject);
+        }
+    }
+
+}
